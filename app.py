5d1be450371eedd0a0b0917af7f9796dcd2ec271
ahmadkhan111100 <43084925+ahmadkhan111100@users.noreply.github.com>
2026-07-14 03:39:37 -0700

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 from datetime import datetime
-import plotly.express as px
 
 # -----------------------------
 # Streamlit Page Configuration
